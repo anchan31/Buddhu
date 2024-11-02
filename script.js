d430fbde356b0ebc41a6790f095d05e614c8b041
anchan31 <151496115+anchan31@users.noreply.github.com>
2024-11-02 20:34:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function startJourney() {
-    window.location.href = "anchan31.github.io/Best_Memories"; // Redirect to the timeline page
+    window.location.href = "/Best_Memories/index.html"; // Redirect to the timeline page
 }
